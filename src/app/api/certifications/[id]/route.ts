dd8ead27f80f8d3fb90369d30c3e536f26c0de55
juan.salinas <juan.salinas@travelx.io>
2024-07-17 17:23:07 -0400

    certifications api

--- src/app/api/certifications/[id]/route.ts
+++ src/app/api/certifications/[id]/route.ts
@@ -1,27 +1,25 @@
-import { NextRequest, NextResponse } from "next/server";
-import { StatusCodes } from "http-status-codes";
+// Importar dependencias
+import { NextRequest, NextResponse } from 'next/server';
+import Airtable from 'airtable';
+import { StatusCodes } from 'http-status-codes';
 
-export async function GET(req : NextRequest,{params}:{params: {id: string}}){
-    const id = params.id
+const base = new Airtable({ apiKey: process.env.DB_API_KEY }).base(process.env.DB_BASE_ID);
+
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+    const id = params.id;
     try {
-        return NextResponse.json(
-            {
-                "id": "recXWNpS7gNF8PFP8",
-                "createdTime": "2024-07-02T23:08:58.000Z",
-                "fields": {
-                    "description": "Certificado de Marketing Digital",
-                    "heading": "Certificado de Marketing Digital",
-                    "detail": "Este certificado es prueba de haber completado el curso de marketing digital",
-                    "image_url": "https://ipfs.io/ipfs/bafybeidl6yn42rwr67bhzv4sgq3kiidzfugl6fgzvnzfvlbha4fbpsun6i",
-                    "blockchain_explorer_url": "https://allo.info/asset/1794400153/nft"
-                }
-            }
-        );
+        const record = await base('certificates').find(id);
+        const response = {
+            id: record.id,
+            createdTime: record._rawJson.createdTime,
+            fields: record.fields,
+        };
+        return NextResponse.json(response);
     } catch (error) {
+        console.error('Error fetching data from Airtable:', error);
         return NextResponse.json(
             error.message,
             {status:StatusCodes.NOT_FOUND}
-        )    
+        )   
     }
-    
 }
